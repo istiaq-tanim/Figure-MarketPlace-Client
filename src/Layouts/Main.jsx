7feb31b5a5e8bf026771c5b16ed78e60de2386e0
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-05-17 21:51:51 +0600

    basic setup

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
+
+const Main = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+           <div className='min-h[calc(100vh-276px)]'><Outlet></Outlet></div>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
